da3645f5e1c38cd40faaa7e4ce393fc7cf75642c
Roman Volozhanin <rsvolozhanin@gmail.com>
2023-09-17 22:27:54 +0400

    correct models import

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,10 +1,3 @@
-from app.models.quizzes.words import (
-    Word,
-    WordInDB,
-    WordSet,
-    WordSetConnector,
-    WordUpdate,
-)
-
+from .quizzes.words import Word, WordInDB, WordSet, WordSetConnector, WordUpdate
 from .tools import responses
 from .users import User, UserIn, UserOut, UserUpdate
